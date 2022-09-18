0e95e576a882cff6489b10e2761d2cd49901651e
Karim-Dorado <karimdorado@gmail.com>
2022-09-18 21:25:00 +0200

    added locust performance test

---
+++ tests/performance_test/locustfile.py
@@ -0,0 +1,27 @@
+from locust import HttpUser, task
+
+class ProjectPerfTest(HttpUser):
+    
+    def on_start(self):
+        self.client.post("showSummary", {"email": "john@simplylift.co"})
+
+    def on_stop(self):
+        self.client.get("logout")
+
+    @task
+    def board(self):
+        self.client.get("board")
+
+    @task
+    def booking(self):
+        name = "Simply Lift"
+        competition = "Fall Classic"
+        self.client.get(f"book/{competition}/{name}")
+
+    @task
+    def purchase(self):
+        self.client.post("purchasePlaces", data={
+            "club": "Simply Lift",
+            "competition": "Fall Classic",
+            "places": 5
+        })
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,9 +1,6 @@
-def board_status_code_ok(client):
-    response = client.get('/board')
-    assert response.status_code == 200
-
 def test_display_board(client):
     response = client.get('/board')
+    assert response.status_code == 200
     assert ('Simply Lift Points: 13') in response.data.decode()
     assert ('Iron Temple Points: 4') in response.data.decode()
     assert ('She Lifts Points: 12') in response.data.decode()
