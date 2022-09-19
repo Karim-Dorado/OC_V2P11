bee8fc0ecdc178b5542760feae58f7da05132184
Karim-Dorado <karimdorado@gmail.com>
2022-09-19 17:29:06 +0200

    add commentaries

--- server.py
+++ server.py
@@ -31,6 +31,8 @@ def index():
 
 @app.route('/showSummary', methods=['POST'])
 def showSummary():
+    # Checks if the email address is listed,
+    # otherwise redirects to the index page with an error message.
     try:
         club = [club for club in clubs if club['email'] == request.form['email']][0]
         return render_template('welcome.html',
@@ -63,19 +65,35 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+
+    # Creates a counter to check that the user
+    # can buy a maximum of 12 places in several times
     if not competition['name'] in club:
         club[competition['name']] = int(0)
+
+    # Checks that the competition date is before today
     if competition['date'] < today:
         flash('This competition is no more available.')
+
+    # Checks that the number of required places is positive
     elif placesRequired <= 0:
         flash("Please, enter a positive number!")
+
+    # Checks if a club try to purchase more than 12 places
     elif placesRequired+club[competition['name']] > 12:
         flash("You cannot require more than 12 places per competition")
+
+    # Checks if a club has enough points to purchase places
     elif int(club['points']) < (COST*placesRequired):
         flash("Not enough points to require this number of places!")
     else:
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+
+    # Updates club points
         club['points'] = int(club['points']) - (COST*placesRequired)
+
+    # Updates the counter to check that the user
+    # can buy a maximum of 12 places in several times
         club[competition['name']] += placesRequired
         flash('Great-booking complete!')
     return render_template('welcome.html',
