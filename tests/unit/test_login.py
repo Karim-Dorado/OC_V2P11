62c5499f25596013e6692a6c3ab1490fff82956c
Karim-Dorado <karimdorado@gmail.com>
2022-09-20 04:13:11 +0200

    move unit tests into unit folder

---
+++ tests/unit/test_login.py
@@ -0,0 +1,17 @@
+def test_valid_email(client):
+    email = "john@simplylift.co"
+    response = client.post('/showSummary', data={"email": email})
+    assert response.status_code == 200
+    assert ("Welcome, " + email) in response.data.decode()
+
+
+def test_invalid_email(client):
+    response = client.post('/showSummary', data={"email": "test@test.com"})
+    assert response.status_code == 200
+    assert ("Unknown email address") in response.data.decode()
+
+
+def test_empty_email(client):
+    response = client.post('/showSummary', data={"email": ""})
+    assert response.status_code == 200
+    assert ("Unknown email address") in response.data.decode()
